f78440ed1d6fbaf74133c9f8080cce7214cfceea
nicholass003 <veganicholas123@gmail.com>
2024-04-06 12:44:38 +0700

    chore: initialize project 🚀

---
+++ src/command/QuantumCrates.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Copyright (c) 2024 - present nicholass003
+ *        _      _           _                ___   ___ ____
+ *       (_)    | |         | |              / _ \ / _ \___ \
+ *  _ __  _  ___| |__   ___ | | __ _ ___ ___| | | | | | |__) |
+ * | '_ \| |/ __| '_ \ / _ \| |/ _` / __/ __| | | | | | |__ <
+ * | | | | | (__| | | | (_) | | (_| \__ \__ \ |_| | |_| |__) |
+ * |_| |_|_|\___|_| |_|\___/|_|\__,_|___/___/\___/ \___/____/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  nicholass003
+ * @link    https://github.com/nicholass003/
+ *
+ *
+ */
+
+declare(strict_types=1);
+
+namespace nicholass003\quantumcrates\command;
+
+use nicholass003\quantumcrates\QuantumCrates;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
+
+class QuantumCratesCommand extends Command implements PluginOwned{
+
+	public function __construct(
+		private QuantumCrates $plugin
+	){
+
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args) : void{
+
+	}
+
+	public function getOwningPlugin() : Plugin{
+		return $this->plugin;
+	}
+}
